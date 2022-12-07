a2ab73a3459b5cd1bc8460f8c89804526f7ac2dc
Naiara Rodrigues <rnaiara92@gmail.com>
2022-12-07 11:39:23 -0300

    cadastrar dados

---
+++ php/cadastrar.php
@@ -0,0 +1,29 @@
+<?php
+    //Incluir conexão
+    include("conexao.php");
+
+    //Obter dados JSON
+    $obterDados = file_get_contents("php: //input");
+
+    //Extrair dados JSON
+    $extrair = json_decode($obterDados);
+
+    //Separar os dados do JSON
+    $nomeCurso = $extrair->cursos->nomeCurso;
+    $valorCurso = $extrair->cursos->valorCurso;
+
+    //SQL
+    $sql = "INSERT INTO cursos(nomeCurso, valorCurso) VALUES('$nomeCurso', $valorCurso)";
+    mysqli_query($conexao, $sql);
+
+    //Exportat os dados cadastrados
+    $curso = [
+        'nomeCurso' => $nomeCurso,
+        'valorCurso' => $valorCurso
+    ]
+
+    json_encode(['curso']=>$curso);
+    
+
+
+?>
